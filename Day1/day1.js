5bcf98e11dcbe049211f41627453245160c7e6a6
Hamim <74880822+hamim23z@users.noreply.github.com>
2023-06-11 15:39:53 -0400

    Create day1.js

---
+++ Day1/day1.js
@@ -0,0 +1,72 @@
+console.log('Hello World')
+
+let firstName
+console.log(firstName) //Undefined//
+
+//Checking Data Types//
+console.log(typeof 'Hamim')
+console.log(typeof 5)
+console.log(typeof true)
+console.log(typeof null)
+console.log(typeof undefined)
+
+//Declared Variables//
+let lastName = 'Malone'
+let age = 100
+console.log(lastName, age)
+
+let person = 'Hamim', job = 'student', place = 'New York City'
+console.log(person, job, place)
+
+
+
+
+
+//Day One Exercises//
+//Number 1//
+//Comments can make code readable//
+
+//Number Two//
+//Welcome to 30DaysOfJavaScript//
+
+//Number Three//
+/*Comments can make code readble, 
+easy to use and informative*/
+
+//Number Four//
+let x = 'Hello'
+let y = false
+let z 
+let h = null
+
+//Number Five//
+console.log(typeof x)
+console.log(typeof y)
+console.log(typeof z)
+console.log(typeof h)
+
+//Number Six//
+let a 
+let b
+let c
+let d
+
+//Number Seven//
+let e = 'egg'
+let f = 'frank'
+let g = 'giant'
+let i = 'indigo'
+
+//Number Eight//
+let fN = 'Hamim'
+let lN = 'Choudhury'
+let status = 'single'
+let digits = 19
+
+//Number 9 is just a repeat of #8 in the same line, so I skipped//
+
+//Number Ten//
+let myAge = 19
+let yourAge = 22
+console.log('I am ' + myAge + ' years old.')
+console.log('You are ' + yourAge + ' years old.')
