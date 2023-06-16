a7a1d48187157f3ad493571eba57843c5ea6c41b
Hamim <74880822+hamim23z@users.noreply.github.com>
2023-06-16 15:25:03 -0400

    Update day4.js

--- Day4/day4.js
+++ Day4/day4.js
@@ -134,3 +134,13 @@ let youragenow = prompt('Enter your age: ')
 let myagenow = 19
 let compareages = youragenow - myagenow
 console.log('You are ' + compareages + ' years older than me.')
+
+
+//Number Three//
+let a = 4
+let b = 3
+if (a > b) {
+    console.log(a + ' is greater than ' + b)
+} else {
+    console.log(a + ' is less than ' + b)
+}
