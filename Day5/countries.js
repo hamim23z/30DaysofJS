ca2a8462036c2be3e477982c2f42cbff95bfdb91
Hamim <74880822+hamim23z@users.noreply.github.com>
2023-06-19 16:27:48 -0400

    Update countries.js

--- Day5/countries.js
+++ Day5/countries.js
@@ -1,1 +1,19 @@
+const countries = [
+    'Albania',
+    'Bolivia',
+    'Canada',
+    'Denmark',
+    'Ethiopia',
+    'Finland',
+    'Germany',
+    'Hungary',
+    'Ireland',
+    'Japan',
+    'Kenya'
+  ]
 
+if (countries.includes('Ethiopia')) {
+  console.log('ETHIOPIA')
+} else {
+  console.log('Ethiopia has been added')
+}
