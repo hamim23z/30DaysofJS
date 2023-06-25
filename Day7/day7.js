25a6d213e8a5b7eaf899a57059af3564799626a2
Hamim <74880822+hamim23z@users.noreply.github.com>
2023-06-25 13:59:53 -0400

    Update day7.js

--- Day7/day7.js
+++ Day7/day7.js
@@ -1,1 +1,34 @@
+//FUNCTIONS//
+//Function Declaration//
+/* function functionName() {
+  // code goes here
+}
+functionName() */
 
+
+//Function without a Parameter and Return//
+function square() {
+    let num = 2
+    let sq = num * num
+    console.log(sq)
+} 
+square() //in order for a function to work, you need to call the function at the end, like i did in this line//
+
+
+function addtwonumbers() {
+    let numone = 10
+    let numtwo = 20
+    let totalsum = numone + numtwo
+    console.log(totalsum)
+}
+addtwonumbers() //same thing, remember to call the function//
+
+
+function fullnameprint() {
+    let first = 'Hamim'
+    let last = 'Choudhury'
+    let ag = 19
+    let printfullname = first + ' ' + last + ', ' + ag
+    console.log(printfullname)
+}
+fullnameprint() //call function//
