a45bad1959b7ab94bca9cc8fc9d390445b3136dd
Hamim <74880822+hamim23z@users.noreply.github.com>
2023-06-30 17:07:24 -0400

    Update interview.js

--- Small Interview Script/interview.js
+++ Small Interview Script/interview.js
@@ -1,1 +1,23 @@
+let question1 = prompt('How did you hear about this position?')
+console.log(question1)
 
+let question2 = prompt('Why did you decide to apply for this position?')
+console.log(question2)
+
+let question3 = prompt('What makes you stand out from other candidates? In other words, what are your strengths and weaknesses that sets you apart?')
+console.log(question3)
+
+let question4 = prompt('What is a professional achievement that you are most proud of?')
+console.log(question4)
+
+let question5 = prompt('Give an example of how you have handled a challenge in the workplace before.')
+console.log(question5)
+
+let question6 = prompt('When you’re balancing multiple projects, how do you keep yourself organized?')
+console.log(question6)
+
+let question7 = prompt('What can you bring to the company?')
+console.log(question7)
+
+let question8 = prompt('Do you have any questions for me?')
+console.log(question8)
