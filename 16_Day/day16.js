38e369cf5631d9ec05ab01d9ee5bb9e9a8f3123d
Hamim <hamimc232@gmail.com>
2023-07-29 16:11:49 -0400

    Update day16.js

--- 16_Day/day16.js
+++ 16_Day/day16.js
@@ -1,1 +1,209 @@
+//JSON//
+// JSON stands for JavaScript Object Notation. The JSON syntax is derived from JavaScript object notation syntax, but the JSON format is text or string only. JSON is a light weight data format for storing and transporting. JSON is mostly used when data is sent from a server to a client. JSON is an easier-to-use alternative to XML. //
 
+//Example of JSON//
+{
+"users";[
+    {
+    "firstName":"Asabeneh",
+    "lastName":"Yetayeh",
+    "age":250,
+    "email":"asab@asb.com"
+    },
+    {
+    "firstName":"Alex",
+    "lastName":"James",
+    "age":25,
+    "email":"alex@alex.com"
+    },
+    {
+    "firstName":"Lidiya",
+    "lastName":"Tekle",
+    "age":28,
+    "email":"lidiya@lidiya.com"
+    }
+]
+}
+
+// The above JSON example is not much different from a normal object. Then, what is the difference ? The difference is THEY KEY OF A JSON OBJECT SHOULD BE WITH DOUBLE QUOTES OR IT SHOULD BE A STRING. //
+
+// Inside the array we have different objects and each objects has keys, each keys has to have double quotes. For instance, we use "firstNaMe" instead of just firstName, however in object we use keys without double quotes. This is the major difference between an object and a JSON. //
+
+
+
+//Converting JSON to JavaScript Object//
+//When we want to change the JSON to an object we parse the JSON using JSON.parse().//
+
+/* Syntax:
+JSON.parse(json[, reviver])
+// json or text , the data
+// reviver is an optional callback function
+// JSON.parse(json, (key, value) => {
+
+})
+*/
+
+let usersText = `{
+"users":[
+    {
+    "firstName":"Asabeneh",
+    "lastName":"Yetayeh",
+    "age":250,
+    "email":"asab@asb.com"
+    },
+    {
+    "firstName":"Alex",
+    "lastName":"James",
+    "age":25,
+    "email":"alex@alex.com"
+    },
+    {
+    "firstName":"Lidiya",
+    "lastName":"Tekle",
+    "age":28,
+    "email":"lidiya@lidiya.com"
+    }
+]
+}`
+
+
+let usersObj = JSON.parse(usersText, undefined, 4)
+console.log(usersObj)
+
+
+
+//Using a reviver function with JSON.parse()//
+//To use the reviver function as a formatter, we put the keys we want to format first name and last name value. Let us say, we are interested to format the firstName and lastName of the JSON data.//
+
+let usersText2 = `{
+"users":[
+    {
+    "firstName":"Asabeneh",
+    "lastName":"Yetayeh",
+    "age":250,
+    "email":"asab@asb.com"
+    },
+    {
+    "firstName":"Alex",
+    "lastName":"James",
+    "age":25,
+    "email":"alex@alex.com"
+    },
+    {
+    "firstName":"Lidiya",
+    "lastName":"Tekle",
+    "age":28,
+    "email":"lidiya@lidiya.com"
+    }
+]
+}`
+    
+let usersObj2 = JSON.parse(usersText2, (key, value) => {
+    let newValue =
+    typeof value == 'string' && key != 'email' ? value.toUpperCase() : value
+    return newValue
+})
+console.log(usersObj2)
+
+
+
+//Converting Object to JSON//
+//When we want to change the object to JSON we use JSON.stringify(). The stringify method takes one required parameter and two optional parameters. The replacer is used as filter and the space is an indentations. If we do not want to filter out any of the keys from the object we can just pass undefined.//
+
+/* Syntax: 
+JSON.stringify(obj, replacer, space)
+// json or text , the data
+// reviver is an optional callback function
+*/
+
+let users = {
+Alex: {
+    email: 'alex@alex.com',
+    skills: ['HTML', 'CSS', 'JavaScript'],
+    age: 20,
+    isLoggedIn: false,
+    points: 30
+},
+Asab: {
+    email: 'asab@asab.com',
+    skills: [
+    'HTML',
+    'CSS',
+    'JavaScript',
+    'Redux',
+    'MongoDB',
+    'Express',
+    'React',
+    'Node'
+    ],
+    age: 25,
+    isLoggedIn: false,
+    points: 50
+},
+Brook: {
+    email: 'daniel@daniel.com',
+    skills: ['HTML', 'CSS', 'JavaScript', 'React', 'Redux'],
+    age: 30,
+    isLoggedIn: true,
+    points: 50
+},
+Daniel: {
+    email: 'daniel@alex.com',
+    skills: ['HTML', 'CSS', 'JavaScript', 'Python'],
+    age: 20,
+    isLoggedIn: false,
+    points: 40
+},
+John: {
+    email: 'john@john.com',
+    skills: ['HTML', 'CSS', 'JavaScript', 'React', 'Redux', 'Node.js'],
+    age: 20,
+    isLoggedIn: true,
+    points: 50
+},
+Thomas: {
+    email: 'thomas@thomas.com',
+    skills: ['HTML', 'CSS', 'JavaScript', 'React'],
+    age: 20,
+    isLoggedIn: false,
+    points: 40
+},
+Paul: {
+    email: 'paul@paul.com',
+    skills: [
+    'HTML',
+    'CSS',
+    'JavaScript',
+    'MongoDB',
+    'Express',
+    'React',
+    'Node'
+    ],
+    age: 20,
+    isLoggedIn: false,
+    points: 40
+}
+}
+  
+let txt = JSON.stringify(users, undefined, 4)
+console.log(txt)
+
+
+
+//Using a Filter Array with JSON.stringify//
+//Now, lets use the replacer as a filter. The user object has long list of keys but we are interested only in few of them. We put the keys we want to keep in array as show in the example and use it the place of the replacer.//
+
+let user2 = {
+    firstName: 'Asabeneh',
+    lastName: 'Yetayeh',
+    country: 'Finland',
+    city: 'Helsinki',
+    email: 'alex@alex.com',
+    skills: ['HTML', 'CSS', 'JavaScript', 'React', 'Python'],
+    age: 250,
+    isLoggedIn: false,
+    points: 30
+}
+  
+let txt2 = JSON.stringify(user2,['firstName', 'lastName', 'country', 'city', 'age'],4)
+console.log(txt2)
